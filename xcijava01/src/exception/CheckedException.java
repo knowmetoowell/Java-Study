702c7e0c155a7c82c8cc5a14e6fc2e721880e685
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2023-10-20 15:02:50 +0900

    Create CheckedException.java

---
+++ xcijava01/src/exception/CheckedException.java
@@ -0,0 +1,24 @@
+package exception;
+
+import builder.Human;
+
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class CheckedException {
+    public static void main(String[] args) {
+//        Thread.sleep(1000);
+//
+//        Class cls = Class.forName("java.lang.Object");
+//
+//        InputStreamReader in = new InputStreamReader(System.in);
+//        in.read();
+//
+//        FileInputStream fis = new FileInputStream("test.txt");
+//
+//        Human h = new Human();
+//        Human h2 = (Human) h.clone(); //clone은 protected라 원래 Human에 못쓰긴함
+
+    }
+}
