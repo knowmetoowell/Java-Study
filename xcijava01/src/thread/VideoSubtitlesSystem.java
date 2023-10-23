f8ab6ce14ac2e64a4b5dca16f4dc443090a1c465
knowmetoowell <87797445+knowmetoowell@users.noreply.github.com>
2023-10-23 09:16:07 +0900

    Create VideoSubtitlesSystem.java

---
+++ xcijava01/src/thread/VideoSubtitlesSystem.java
@@ -0,0 +1,54 @@
+package thread;
+
+class SMFileThread extends Thread {
+    @Override
+    public void run() {
+        String[] strArray = {"하나", "둘", "셋", "넷", "다섯"};
+        try {
+            Thread.sleep(100);
+        } catch (Exception e) {
+
+        }
+
+        for (int i = 0; i < strArray.length; i++) {
+            System.out.println("- (자막 번호) " + strArray[i]);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                //throw new RuntimeException(e);
+            }
+
+        }
+    }
+}
+
+class VideoFileThread extends Thread {
+    @Override
+    public void run() {
+        int[] intArr = {1, 2, 3, 4, 5};
+
+        for (int i = 0; i < intArr.length; i++) {
+            System.out.print("(비디오 프레임) " + intArr[i]);
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                //throw new RuntimeException(e);
+            }
+
+        }
+    }
+}
+
+
+
+public class VideoSubtitlesSystem {
+    public static void main(String[] args) {
+        Thread th = new SMFileThread();
+        th.start();
+
+        Thread th2 = new VideoFileThread();
+        th2.start();
+
+
+    }
+}
